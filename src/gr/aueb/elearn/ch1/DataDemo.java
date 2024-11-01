545327955f02dad008d0233023540ec035320914
AnDagkalakis <andagalakis@gmail.com>
2024-11-01 23:31:25 +0200

    Add Demo v 0.2

--- src/gr/aueb/elearn/ch1/DataDemo.java
+++ src/gr/aueb/elearn/ch1/DataDemo.java
@@ -5,6 +5,15 @@ package gr.aueb.elearn.ch1;
 
 
 public class DataDemo {
-    
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 =10;
+        int result = num1 + num2;
+
+        System.out.printf("The result %d and %d = %d \n" ,num1 ,num2 ,result);
+
+    }
 
 }
